bd5e5e1085d6622408fca053f4a4bec72abd25bf
Vener Fruet da Silveira <105865020+venerfruet@users.noreply.github.com>
2022-08-01 18:41:01 -0300

    Add files via upload

---
+++ precache-manifest.be4d5b055d1f7edb73312dfd8561d762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "37d199acb258ca70c42d",
+    "url": "/githubfelix/static/js/main.37d199ac.chunk.js"
+  },
+  {
+    "revision": "d7df22e711c646de8e28",
+    "url": "/githubfelix/static/js/1.d7df22e7.chunk.js"
+  },
+  {
+    "revision": "b5124dceb287ec880cb6",
+    "url": "/githubfelix/static/js/runtime~main.b5124dce.js"
+  },
+  {
+    "revision": "c9c955f32a0480398ea73a3cf17d7b88",
+    "url": "/githubfelix/static/media/felix.c9c955f3.png"
+  },
+  {
+    "revision": "e2d2235eb01f1eeb084a40e20d65dd03",
+    "url": "/githubfelix/static/media/logo-felix.e2d2235e.png"
+  },
+  {
+    "revision": "1d73d31df7999677fc854c5b6a6eb80f",
+    "url": "/githubfelix/index.html"
+  }
+];
